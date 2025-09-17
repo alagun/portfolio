c27c9c9314e537d995b86ee82afe2e979b08c024
Alexey Lagun <alexey.lagun@gmail.com>
2025-09-17 16:54:17 +0300

    fix: подключение стилей

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { getCustomTheme } from '@/shared/config/antd-theme'
 import { ThemeToggle } from '@/features/theme/theme-toggle/ThemeToggle'
 import { selectTheme } from './store/selectors'
 import styles from './styles/App.module.scss'
+import "./styles/global.scss";
 
 const { Header, Content } = Layout
 
@@ -21,8 +22,8 @@ export default function App () {
       <Layout className={styles.layout}>
         <Header className={styles.header}>
           <Link to='/' className={styles.logo}>
-            <span className={styles.logoIcon}>Y</span>
-            <span className={styles.logoText}>Portfolio</span>
+            <span className={styles.logoIcon}>i</span>
+            <span className={styles.logoText}>Alexey Lagun</span>
           </Link>
 
           <div className={styles.headerRight}>
