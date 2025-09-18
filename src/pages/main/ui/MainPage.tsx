f06676a6c244f7aed04e185cd95f82df854ff378
Alexey Lagun <alexey.lagun@gmail.com>
2025-09-18 12:13:08 +0300

    fix: реимпорт

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -1,12 +1,31 @@
-import { Layout } from 'antd'
-
+import { Row, Col } from 'antd'
+import { 
+  HeroSection, 
+  SkillsSection, 
+  ExperienceSection, 
+  EducationSection, 
+  ProjectsSection, 
+  AdditionalInfoSection 
+} from '@/widgets'
 import styles from './MainPage.module.scss'
-
 
 export const MainPage = () => {
   return (
-    <Layout className={styles.layout}>
-      Главная
-    </Layout>
+    <div className={styles.mainPage}>
+      <HeroSection />
+      <SkillsSection />
+      <ExperienceSection />
+      
+      <Row gutter={[16, 16]}>
+        <Col xs={24} md={12}>
+          <EducationSection />
+        </Col>
+        <Col xs={24} md={12}>
+          <ProjectsSection />
+        </Col>
+      </Row>
+      
+      <AdditionalInfoSection />
+    </div>
   )
 }
---
+++ src/widgets/index.ts
@@ -0,0 +1,6 @@
+export { HeroSection } from './hero-section'
+export { SkillsSection } from './skills-section'
+export { ExperienceSection } from './experience-section'
+export { EducationSection } from './education-section'
+export { ProjectsSection } from './projects-section'
+export { AdditionalInfoSection } from './additional-info-section'
