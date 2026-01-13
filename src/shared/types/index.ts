52420ce88650888998433e052a7d19691b5eba10
Alexey Lagun <alexey.lagun@gmail.com>
2026-01-13 16:52:03 +0300

    fix: исправлен интерфейс

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,3 +1,3 @@
-export { Certificate, CertificateModalState } from './certificates/types'
+export { ICertificate, CertificateModalState } from './certificates/types'
 export { IProjects, IProject  } from './projects/projects'
 export { ISkills, ISkill  } from './skills/skills'
